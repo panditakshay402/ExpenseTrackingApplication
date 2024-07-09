73413b71bc59fad05720bb19f5af3d2917692c4f
JaroslawX <ad.jaro@wp.pl>
2024-07-09 09:37:23 +0200

    Initial commit

---
+++ ExpenseTrackingApplication/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace ExpenseTrackingApplication.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
