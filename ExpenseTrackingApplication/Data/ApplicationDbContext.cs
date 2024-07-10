8f6fd61df7332f9319fde8c3594896fcad69a28d
JaroslawX <ad.jaro@wp.pl>
2024-07-10 12:25:26 +0200

    Update, add database

---
+++ ExpenseTrackingApplication/Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using ExpenseTrackingApplication.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ExpenseTrackingApplication.Data;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+    {
+        
+    }
+    public DbSet<Budget> Budgets { get; set; }
+    public DbSet<Transaction> Transactions { get; set; }
+    public DbSet<Notification> Notifications { get; set; }
+    public DbSet<Report> Reports { get; set; }
+    public DbSet<Security> Securities { get; set; }
+    
+}
