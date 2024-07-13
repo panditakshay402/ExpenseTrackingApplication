af70f405a0fed51aa5f74f9b6efaa54d01cb5970
JaroslawX <ad.jaro@wp.pl>
2024-07-13 10:32:25 +0200

    Create notification

---
+++ ExpenseTrackingApplication/Controllers/NotificationController.cs
@@ -0,0 +1,22 @@
+﻿using ExpenseTrackingApplication.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace ExpenseTrackingApplication.Controllers;
+
+public class NotificationController : Controller
+{
+    private readonly ApplicationDbContext _context;
+
+    public NotificationController(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    // GET: Notifications
+    public async Task<IActionResult> Index()
+    {
+        var notifications = await _context.Notifications.ToListAsync();
+        return View(notifications);
+    }
+}
