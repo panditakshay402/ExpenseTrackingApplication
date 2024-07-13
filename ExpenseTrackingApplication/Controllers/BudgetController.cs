05993a5bb3bb306f10f05c6a344c74940a54d0f5
JaroslawX <ad.jaro@wp.pl>
2024-07-13 11:26:01 +0200

    Update budget, fixes

--- ExpenseTrackingApplication/Controllers/BudgetController.cs
+++ ExpenseTrackingApplication/Controllers/BudgetController.cs
@@ -1,10 +1,13 @@
-﻿using ExpenseTrackingApplication.Data;
+﻿using System.Security.Claims;
+using ExpenseTrackingApplication.Data;
 using ExpenseTrackingApplication.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace ExpenseTrackingApplication.Controllers;
 
+[Authorize]
 public class BudgetController : Controller
 {
     private readonly ApplicationDbContext _context;
@@ -16,7 +19,15 @@ public class BudgetController : Controller
     // GET: Budget
     public async Task<IActionResult> Index()
     {
-        return View(await _context.Budgets.ToListAsync());
+        // Get the Id of the currently logged-in user
+        var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+
+        // Get all budgets that belong to the currently logged-in user
+        var budgets = await _context.Budgets
+            .Where(b => b.AppUserId == userId)
+            .ToListAsync();
+
+        return View(budgets);
     }
     
     // GET: Budget/Create
@@ -32,6 +43,12 @@ public class BudgetController : Controller
     {
         if (ModelState.IsValid)
         {
+            // Get the Id of the currently logged-in user
+            var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+        
+            // Set the AppUserId of the budget to the ID of the currently logged-in user
+            budget.AppUserId = userId;
+
             _context.Add(budget);
             await _context.SaveChangesAsync();
             return RedirectToAction(nameof(Index));
