12bff0deeb436ec8e0063cf5b66bd8b851080ee4
JaroslawX <ad.jaro@wp.pl>
2024-07-16 10:35:42 +0200

    Update, more methods, changes to async

--- ExpenseTrackingApplication/Interfaces/ITransactionRepository.cs
+++ ExpenseTrackingApplication/Interfaces/ITransactionRepository.cs
@@ -5,11 +5,16 @@ namespace ExpenseTrackingApplication.Interfaces;
 
 public interface ITransactionRepository
 {
+    // TODO: Change everything to async?
     Task<IEnumerable<Transaction?>> GetAll();
     Task<Transaction?> GetById(int id);
+    Task<IEnumerable<Transaction>> GetTransactionByUser(string userId);
     Task<IEnumerable<Transaction>> GetTransactionByCategory(TransactionCategory category);
-    bool Add(Transaction transaction);
-    bool Delete(Transaction transaction);
-    bool Update(Transaction transaction);
-    bool Save();
+    Task<IEnumerable<Transaction>> GetTransactionsByDateRange(string userId, DateTime startDate, DateTime endDate);
+    Task<decimal> GetTotalAmountByCategory(string userId, TransactionCategory category);
+    Task<int> GetTransactionCountByUser(string userId);
+    Task<bool> AddAsync(Transaction transaction);
+    Task<bool> DeleteAsync(Transaction transaction);
+    Task<bool> UpdateAsync(Transaction transaction);
+    Task<bool> SaveAsync();
 }
--- ExpenseTrackingApplication/Repositories/TransactionRepository.cs
+++ ExpenseTrackingApplication/Repositories/TransactionRepository.cs
@@ -22,33 +22,55 @@ public class TransactionRepository : ITransactionRepository
     {
         return await _context.Transactions.FirstOrDefaultAsync(t => t.Id == id);
     }
-
+    
+    public async Task<IEnumerable<Transaction>> GetTransactionByUser(string userId)
+    {
+        return await _context.Transactions.Where(t => t.AppUserId == userId).ToListAsync();
+    }
+    
     public async Task<IEnumerable<Transaction>> GetTransactionByCategory(TransactionCategory category)
     {
         return await _context.Transactions.Where(t => t.Category == category).ToListAsync();
     }
+    
+    public async Task<IEnumerable<Transaction>> GetTransactionsByDateRange(string userId, DateTime startDate, DateTime endDate)
+    {
+        return await _context.Transactions.Where(t => t.AppUserId == userId && t.Date >= startDate && t.Date <= endDate)
+            .ToListAsync();
+    }
+    
+    public async Task<decimal> GetTotalAmountByCategory(string userId, TransactionCategory category)
+    {
+        return (decimal)await _context.Transactions
+            .Where(t => t.AppUserId == userId && t.Category == category)
+            .SumAsync(t => t.Amount);
+    }
+    
+    public async Task<int> GetTransactionCountByUser(string userId)
+    {
+        return await _context.Transactions.CountAsync(t => t.AppUserId == userId);
+    }
+    
+    public async Task<bool> AddAsync(Transaction transaction)
+    {
+        await _context.Transactions.AddAsync(transaction);
+        return true;
+    }
 
-    public bool Add(Transaction transaction)
+    public async Task<bool> DeleteAsync(Transaction transaction)
     {
-        _context.Add(transaction);
-        return Save();
+        _context.Transactions.Remove(transaction);
+        return await SaveAsync();
     }
-    
-    public bool Delete(Transaction transaction)
+
+    public async Task<bool> UpdateAsync(Transaction transaction)
     {
-        _context.Remove(transaction);
-        return Save();
+        _context.Transactions.Update(transaction);
+        return await SaveAsync();
     }
-    
-    public bool Update(Transaction transaction)
+
+    public async Task<bool> SaveAsync()
     {
-        _context.Update(transaction);
-        return Save();
-    }
-    
-    public bool Save()
-    {
-        var saved = _context.SaveChanges();
-        return saved > 0 ? true : false;
+        return await _context.SaveChangesAsync() > 0;
     }
 }
