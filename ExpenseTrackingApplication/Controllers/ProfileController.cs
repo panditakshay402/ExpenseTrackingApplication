3df442844976611174e26f9896ecbf5a00073ea9
JaroslawX <ad.jaro@wp.pl>
2024-07-16 09:48:40 +0200

    User profile

---
+++ ExpenseTrackingApplication/Controllers/ProfileController.cs
@@ -0,0 +1,23 @@
+﻿using ExpenseTrackingApplication.Data;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ExpenseTrackingApplication.Controllers;
+
+[Authorize]
+public class ProfileController : Controller
+{
+    private readonly ApplicationDbContext _context;
+    public ProfileController(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+    
+    // GET: Profile
+    public IActionResult Index()
+    {
+        return View();
+    }
+    
+    
+}
