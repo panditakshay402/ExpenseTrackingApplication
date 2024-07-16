6e112052ee94320afeb73307cb91f05cc2c74e06
JaroslawX <ad.jaro@wp.pl>
2024-07-16 10:13:20 +0200

    Interfaces and Repositories

---
+++ ExpenseTrackingApplication/Interfaces/ITransactionRepository.cs
@@ -0,0 +1,15 @@
+﻿using ExpenseTrackingApplication.Data.Enum;
+using ExpenseTrackingApplication.Models;
+
+namespace ExpenseTrackingApplication.Interfaces;
+
+public interface ITransactionRepository
+{
+    Task<IEnumerable<Transaction?>> GetAll();
+    Task<Transaction?> GetById(int id);
+    Task<IEnumerable<Transaction>> GetTransactionByCategory(TransactionCategory category);
+    bool Add(Transaction transaction);
+    bool Delete(Transaction transaction);
+    bool Update(Transaction transaction);
+    bool Save();
+}
---
+++ ExpenseTrackingApplication/Repositories/TransactionRepository.cs
@@ -0,0 +1,54 @@
+﻿using ExpenseTrackingApplication.Data;
+using ExpenseTrackingApplication.Data.Enum;
+using ExpenseTrackingApplication.Interfaces;
+using ExpenseTrackingApplication.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ExpenseTrackingApplication.Repositories;
+
+public class TransactionRepository : ITransactionRepository
+{
+    private readonly ApplicationDbContext _context;
+    public TransactionRepository(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+    public async Task<IEnumerable<Transaction?>> GetAll()
+    {
+        return await _context.Transactions.ToListAsync();
+    }
+
+    public async Task<Transaction?> GetById(int id)
+    {
+        return await _context.Transactions.FirstOrDefaultAsync(t => t.Id == id);
+    }
+
+    public async Task<IEnumerable<Transaction>> GetTransactionByCategory(TransactionCategory category)
+    {
+        return await _context.Transactions.Where(t => t.Category == category).ToListAsync();
+    }
+
+    public bool Add(Transaction transaction)
+    {
+        _context.Add(transaction);
+        return Save();
+    }
+    
+    public bool Delete(Transaction transaction)
+    {
+        _context.Remove(transaction);
+        return Save();
+    }
+    
+    public bool Update(Transaction transaction)
+    {
+        _context.Update(transaction);
+        return Save();
+    }
+    
+    public bool Save()
+    {
+        var saved = _context.SaveChanges();
+        return saved > 0 ? true : false;
+    }
+}
