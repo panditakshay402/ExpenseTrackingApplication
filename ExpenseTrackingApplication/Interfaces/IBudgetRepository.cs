c7e2b4266b9e48f71436f472fce4723e260cfd92
JaroslawX <ad.jaro@wp.pl>
2024-07-17 12:07:41 +0200

    Budget repository

---
+++ ExpenseTrackingApplication/Interfaces/IBudgetRepository.cs
@@ -0,0 +1,14 @@
+﻿using ExpenseTrackingApplication.Models;
+
+namespace ExpenseTrackingApplication.Interfaces;
+
+public interface IBudgetRepository
+{
+    Task<IEnumerable<Budget?>> GetAll();
+    Task<Budget?> GetById(int id);
+    Task<IEnumerable<Budget>> GetBudgetByUser(string userId);
+    Task<bool> AddAsync(Budget budget);
+    Task<bool> DeleteAsync(Budget budget);
+    Task<bool> UpdateAsync(Budget budget);
+    Task<bool> SaveAsync();
+}
---
+++ ExpenseTrackingApplication/Repositories/BudgetRepository.cs
@@ -0,0 +1,52 @@
+﻿using ExpenseTrackingApplication.Data;
+using ExpenseTrackingApplication.Interfaces;
+using ExpenseTrackingApplication.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ExpenseTrackingApplication.Repositories;
+
+public class BudgetRepository : IBudgetRepository
+{
+    private readonly ApplicationDbContext _context;
+    public BudgetRepository(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+    public async Task<IEnumerable<Budget?>> GetAll()
+    {
+        return await _context.Budgets.ToListAsync();
+    }
+
+    public async Task<Budget?> GetById(int id)
+    {
+        return await _context.Budgets.FirstOrDefaultAsync(b => b.Id == id);
+    }
+
+    public async Task<IEnumerable<Budget>> GetBudgetByUser(string userId)
+    {
+        return await _context.Budgets.Where(b => b.AppUserId == userId).ToListAsync();
+    }
+
+    public async Task<bool> AddAsync(Budget budget)
+    {
+        await _context.Budgets.AddAsync(budget);
+        return true;
+    }
+
+    public async Task<bool> DeleteAsync(Budget budget)
+    {
+        _context.Budgets.Remove(budget);
+        return true;
+    }
+
+    public async Task<bool> UpdateAsync(Budget budget)
+    {
+        _context.Budgets.Update(budget);
+        return await SaveAsync();
+    }
+
+    public async Task<bool> SaveAsync()
+    {
+        return await _context.SaveChangesAsync() > 0;
+    }
+}
