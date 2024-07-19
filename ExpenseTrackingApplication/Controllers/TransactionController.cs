d5039de9012f2c3cbae4119fe0d0fb35446b46bd
JaroslawX <ad.jaro@wp.pl>
2024-07-19 13:01:20 +0200

    Delete and Edit transaction

--- ExpenseTrackingApplication/Controllers/TransactionController.cs
+++ ExpenseTrackingApplication/Controllers/TransactionController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Security.Claims;
 using ExpenseTrackingApplication.Interfaces;
 using ExpenseTrackingApplication.Models;
+using ExpenseTrackingApplication.ViewModels;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
@@ -69,5 +70,80 @@ public class TransactionController : Controller
         }
 
         return View(transaction);
+    }
+    
+    // GET: Transaction/Edit/{id}
+    public async Task<IActionResult> Edit(int id)
+    {
+        var transaction = await _transactionRepository.GetByIdAsync(id);
+        if (transaction == null)
+        {
+            return NotFound();
+        }
+
+        var transactionViewModel = new EditTransactionViewModel
+        {
+            Id = transaction.Id,
+            Category = transaction.Category,
+            Amount = transaction.Amount,
+            Date = transaction.Date,
+            Description = transaction.Description,
+            AppUserId = transaction.AppUserId
+        };
+
+        return View(transactionViewModel);
+    }
+    
+    // POST: Transaction/Edit/{id}
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Edit(int id, EditTransactionViewModel transactionViewModel)
+    {
+        if (!ModelState.IsValid)
+        {
+            ModelState.AddModelError("", "Failed to edit transaction.");
+            return View("Edit", transactionViewModel);
+        }
+
+        var transaction = await _transactionRepository.GetByIdAsync(id);
+        if (transaction == null)
+        {
+            return NotFound();
+        }
+
+        transaction.Category = transactionViewModel.Category;
+        transaction.Amount = transactionViewModel.Amount;
+        transaction.Date = transactionViewModel.Date;
+        transaction.Description = transactionViewModel.Description;
+
+        await _transactionRepository.UpdateAsync(transaction);
+
+        return RedirectToAction("Index");
+    }
+    
+    // GET: Transaction/Delete/{id}
+    public async Task<IActionResult> Delete(int id)
+    {
+        var transactionDetails = await _transactionRepository.GetByIdAsync(id);
+        if (transactionDetails == null)
+        {
+            return NotFound();
+        }
+
+        return View(transactionDetails);
+    }
+    
+    // POST: Transaction/Delete/{id}
+    [HttpPost, ActionName("DeleteTransaction")]
+    public async Task<IActionResult> DeleteConfirmed(int id)
+    {
+        var transaction = await _transactionRepository.GetByIdAsync(id);
+        if (transaction == null)
+        {
+            return NotFound();
+        }
+
+        await _transactionRepository.DeleteAsync(transaction);
+        return RedirectToAction("Index");
     }
 }
