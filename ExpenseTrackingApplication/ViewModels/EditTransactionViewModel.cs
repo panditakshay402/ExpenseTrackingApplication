82ad552ea637c3709a52d117b6de046cfe12d91a
JaroslawX <ad.jaro@wp.pl>
2024-07-19 13:01:33 +0200

    TransactionViewModel

---
+++ ExpenseTrackingApplication/ViewModels/EditTransactionViewModel.cs
@@ -0,0 +1,13 @@
+﻿using ExpenseTrackingApplication.Data.Enum;
+
+namespace ExpenseTrackingApplication.ViewModels;
+
+public class EditTransactionViewModel
+{
+    public int? Id { get; set; }
+    public TransactionCategory Category { get; set; }
+    public float Amount { get; set; }
+    public DateTime Date { get; set; }
+    public string? Description { get; set; }
+    public string? AppUserId {  get; set; }
+}
