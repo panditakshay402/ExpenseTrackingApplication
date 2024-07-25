285ff8cb7815ba2aec38377f4b766e33ab42780c
JaroslawX <ad.jaro@wp.pl>
2024-07-25 15:04:14 +0200

    Registration date

--- ExpenseTrackingApplication/Models/AppUser.cs
+++ ExpenseTrackingApplication/Models/AppUser.cs
@@ -8,4 +8,5 @@ public class AppUser : IdentityUser
     public ICollection<Transaction>? Transactions { get; set; }
     public ICollection<Report>? Reports { get; set; }
     public Currency PreferredCurrency { get; set; }
+    public DateTime RegistrationDate { get; set; }
 }
