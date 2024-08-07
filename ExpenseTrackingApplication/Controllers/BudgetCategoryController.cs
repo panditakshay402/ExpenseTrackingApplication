401a22ba3fdacc5584885affaa0ded6c2b8e330b
JaroslawX <ad.jaro@wp.pl>
2024-08-07 11:46:48 +0200

    Edit and Delete budgetcategory

--- ExpenseTrackingApplication/Controllers/BudgetCategoryController.cs
+++ ExpenseTrackingApplication/Controllers/BudgetCategoryController.cs
@@ -4,6 +4,8 @@ using ExpenseTrackingApplication.Interfaces;
 using ExpenseTrackingApplication.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
 
 namespace ExpenseTrackingApplication.Controllers;
 
@@ -13,13 +15,14 @@ public class BudgetCategoryController : Controller
     private readonly IBudgetCategoryRepository _budgetCategoryRepository;
     private readonly IBudgetRepository _budgetRepository;
     private readonly ITransactionRepository _transactionRepository;
+    private readonly IIncomeRepository _incomeRepository;
 
-    public BudgetCategoryController(IBudgetCategoryRepository budgetCategoryRepository, IBudgetRepository budgetRepository, ITransactionRepository transactionRepository)
+    public BudgetCategoryController(IBudgetCategoryRepository budgetCategoryRepository, IBudgetRepository budgetRepository, ITransactionRepository transactionRepository, IIncomeRepository incomeRepository)
     {
         _budgetCategoryRepository = budgetCategoryRepository;
         _budgetRepository = budgetRepository;
         _transactionRepository = transactionRepository;
-        
+        _incomeRepository = incomeRepository;
     }
     
     // GET: BudgetCategory/AddNewBudgetCategory
@@ -56,21 +59,24 @@ public class BudgetCategoryController : Controller
     // GET: BudgetCategory/Edit/5
     public async Task<IActionResult> Edit(int id)
     {
-        var budgetCategory = await _budgetCategoryRepository.GetByIdAsync(id);
-        if (budgetCategory == null)
+        var category = await _budgetCategoryRepository.GetByIdAsync(id);
+        if (category == null)
         {
             return NotFound();
         }
 
-        return View(budgetCategory);
+        ViewBag.IncomeCategories = Enum.GetValues(typeof(IncomeCategory)).Cast<IncomeCategory>().ToList();
+        ViewBag.TransactionCategories = Enum.GetValues(typeof(TransactionCategory)).Cast<TransactionCategory>().ToList();
+
+        return View(category);
     }
 
     // POST: BudgetCategory/Edit/5
     [HttpPost]
     [ValidateAntiForgeryToken]
-    public async Task<IActionResult> Edit(int id, BudgetCategory budgetCategory)
+    public async Task<IActionResult> Edit(int id, BudgetCategory category, string[] subCategories)
     {
-        if (id != budgetCategory.Id)
+        if (id != category.Id)
         {
             return NotFound();
         }
@@ -79,15 +85,17 @@ public class BudgetCategoryController : Controller
         {
             try
             {
-                var result = await _budgetCategoryRepository.UpdateAsync(budgetCategory);
-                if (result)
-                {
-                    return RedirectToAction("Details", "Budget", new { id = budgetCategory.BudgetId });
-                }
+                category.SubCategory = string.Join(",", subCategories);
+                
+                // Calculate CurrentBalance
+                category.CurrentBalance = await CalculateCurrentBalance(category);
+
+                await _budgetCategoryRepository.UpdateAsync(category);
+                return RedirectToAction("Details", "Budget", new { id = category.BudgetId });
             }
-            catch (Exception)
+            catch (DbUpdateConcurrencyException)
             {
-                if (await _budgetCategoryRepository.GetByIdAsync(id) == null)
+                if (!await BudgetCategoryExists(category.Id))
                 {
                     return NotFound();
                 }
@@ -96,32 +104,67 @@ public class BudgetCategoryController : Controller
                     throw;
                 }
             }
-
-            ModelState.AddModelError("", "Error while updating category.");
         }
 
-        return View(budgetCategory);
+        ViewBag.IncomeCategories = Enum.GetValues(typeof(IncomeCategory)).Cast<IncomeCategory>().ToList();
+        ViewBag.TransactionCategories = Enum.GetValues(typeof(TransactionCategory)).Cast<TransactionCategory>().ToList();
+
+        return View(category);
+    }
+    
+    private async Task<decimal> CalculateCurrentBalance(BudgetCategory category)
+    {
+        if (category.Type == BudgetCategoryType.Incomes)
+        {
+            var incomes = await _incomeRepository.GetByCategoryAsync(category.BudgetId, category.SubCategory);
+            return incomes.Sum(i => i.Amount);
+        }
+        else if (category.Type == BudgetCategoryType.Expenses)
+        {
+            var transactions = await _transactionRepository.GetByCategoryAsync(category.BudgetId, category.SubCategory);
+            return transactions.Sum(t => t.Amount);
+        }
+
+        return 0;
     }
 
-    // POST: BudgetCategory/Delete/5
-    [HttpPost]
-    [ValidateAntiForgeryToken]
-    public async Task<IActionResult> Delete(int id, int budgetId)
+    private async Task<bool> BudgetCategoryExists(int id)
     {
-        var budgetCategory = await _budgetCategoryRepository.GetByIdAsync(id);
-        if (budgetCategory == null)
+        var category = await _budgetCategoryRepository.GetByIdAsync(id);
+        return category != null;
+    }
+    
+    // GET: BudgetCategory/Delete/5
+    public async Task<IActionResult> Delete(int id)
+    {
+        var category = await _budgetCategoryRepository.GetByIdAsync(id);
+        if (category == null)
         {
             return NotFound();
         }
 
-        var result = await _budgetCategoryRepository.DeleteAsync(budgetCategory);
+        return View(category);
+    }
+    
+    // POST: BudgetCategory/Delete/5
+    [HttpPost, ActionName("Delete")]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> DeleteConfirmed(int id)
+    {
+        var category = await _budgetCategoryRepository.GetByIdAsync(id);
+        if (category == null)
+        {
+            return NotFound();
+        }
+
+        var result = await _budgetCategoryRepository.DeleteAsync(category);
         if (result)
         {
-            return RedirectToAction("Details", "Budget", new { id = budgetId });
+            return RedirectToAction("Details", "Budget", new { id = category.BudgetId });
         }
 
         ModelState.AddModelError("", "Error while deleting category.");
-        return RedirectToAction("Details", "Budget", new { id = budgetId });
+        return RedirectToAction("Details", "Budget", new { id = category.BudgetId });
     }
     
 }
