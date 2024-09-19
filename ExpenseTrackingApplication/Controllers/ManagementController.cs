52a07b2143bcf81c4f64b41d9456079a1f19d10b
JaroslawX <ad.jaro@wp.pl>
2024-09-19 12:50:01 +0200

    AdminPanel management

---
+++ ExpenseTrackingApplication/Controllers/ManagementController.cs
@@ -0,0 +1,40 @@
+﻿using System.Security.Claims;
+using ExpenseTrackingApplication.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ExpenseTrackingApplication.Controllers;
+
+[Authorize(Roles = "Admin")]
+public class ManagementController : Controller
+{
+    private readonly IUserRepository _userRepository;
+    
+    public ManagementController(IUserRepository userRepository)
+    {
+        _userRepository = userRepository;
+    }
+    
+    public async Task<IActionResult> Index()
+    {
+        var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+
+        if (userId == null)
+        {
+            return RedirectToAction("Login", "Account");
+        }
+        
+        var adminProfile = await _userRepository.GetByIdAsync(userId);
+        return View(adminProfile);
+    }
+
+    public IActionResult ManageUsers()
+    {
+        return View();
+    }
+
+    public IActionResult ManageNotifications()
+    {
+        return View();
+    }
+}
