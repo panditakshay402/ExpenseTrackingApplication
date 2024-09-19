92dd27da8d9d62c52c49cd528e8244dc9c19904a
JaroslawX <ad.jaro@wp.pl>
2024-09-19 12:49:31 +0200

    refactor UserRoles, management link in layout

--- ExpenseTrackingApplication/Data/UserRoles.cs
+++ ExpenseTrackingApplication/Data/UserRoles.cs
@@ -2,6 +2,6 @@
 
 public class UserRoles
 {
-    public const string Admin = "admin";
-    public const string User = "user";
+    public const string Admin = "Admin";
+    public const string User = "User";
 }
