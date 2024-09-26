a3438bf4746e2f2312d29270b612d941b963cc08
JaroslawX <ad.jaro@wp.pl>
2024-09-26 11:59:33 +0200

    Interface and repository for BCTC

---
+++ ExpenseTrackingApplication/Interfaces/IBudgetCategoryTransactionCategoryRepository.cs
@@ -0,0 +1,13 @@
+﻿using ExpenseTrackingApplication.Models;
+
+namespace ExpenseTrackingApplication.Interfaces;
+
+public interface IBudgetCategoryTransactionCategoryRepository
+{
+    Task<IEnumerable<BudgetCategoryTransactionCategory?>> GetAllAsync();
+    Task<BudgetCategoryTransactionCategory?> GetByIdAsync(int id);
+    Task<List<BudgetCategoryTransactionCategory>> GetByBudgetCategoryIdAsync(int budgetCategoryId);
+    Task<bool> AddAsync(BudgetCategoryTransactionCategory bCtc);
+    Task<bool> UpdateAsync(BudgetCategoryTransactionCategory bCtc);
+    Task<bool> DeleteAsync(BudgetCategoryTransactionCategory bCtc);
+}
---
+++ ExpenseTrackingApplication/Repositories/BudgetCategoryTransactionCategoryRepository.cs
@@ -0,0 +1,52 @@
+﻿using ExpenseTrackingApplication.Data;
+using ExpenseTrackingApplication.Interfaces;
+using ExpenseTrackingApplication.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ExpenseTrackingApplication.Repositories;
+
+public class BudgetCategoryTransactionCategoryRepository : IBudgetCategoryTransactionCategoryRepository
+{
+    private readonly ApplicationDbContext _context;
+
+    public BudgetCategoryTransactionCategoryRepository(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+    
+    public async Task<IEnumerable<BudgetCategoryTransactionCategory?>> GetAllAsync()
+    {
+        return await _context.BudgetCategoryTransactionCategories.ToListAsync();
+    }
+
+    public async Task<BudgetCategoryTransactionCategory?> GetByIdAsync(int id)
+    {
+        return await _context.BudgetCategoryTransactionCategories.FirstOrDefaultAsync(bctc => bctc.Id == id);
+    }
+
+    public async Task<List<BudgetCategoryTransactionCategory>> GetByBudgetCategoryIdAsync(int budgetCategoryId)
+    {
+        return await _context.BudgetCategoryTransactionCategories
+            .Where(bctc => bctc.BudgetCategoryId == budgetCategoryId)
+            .ToListAsync();
+    }
+
+    public async Task<bool> AddAsync(BudgetCategoryTransactionCategory bCtc)
+    {
+        await _context.BudgetCategoryTransactionCategories.AddAsync(bCtc);
+        return await _context.SaveChangesAsync() > 0;
+    }
+    
+    public async Task<bool> DeleteAsync(BudgetCategoryTransactionCategory bCtc)
+    {
+        _context.BudgetCategoryTransactionCategories.Remove(bCtc);
+        return await _context.SaveChangesAsync() > 0;
+    }
+    
+    public async Task<bool> UpdateAsync(BudgetCategoryTransactionCategory bCtc)
+    {
+        _context.BudgetCategoryTransactionCategories.Update(bCtc);
+        return await _context.SaveChangesAsync() > 0;
+    }
+    
+}
