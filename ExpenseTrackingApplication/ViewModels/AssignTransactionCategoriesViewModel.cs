e0a9e91654b9d7a13847da0ab8395248deae8358
JaroslawX <ad.jaro@wp.pl>
2024-09-27 11:25:34 +0200

    list of strings

--- ExpenseTrackingApplication/ViewModels/AssignTransactionCategoriesViewModel.cs
+++ ExpenseTrackingApplication/ViewModels/AssignTransactionCategoriesViewModel.cs
@@ -5,6 +5,7 @@ namespace ExpenseTrackingApplication.ViewModels;
 public class AssignTransactionCategoriesViewModel
 {
     public int BudgetCategoryId { get; set; }
-    public List<TransactionCategory> AllTransactionCategories { get; set; }
-    public List<TransactionCategory> SelectedCategories { get; set; }
+    public List<string> AllTransactionCategories { get; set; }
+    public List<string> SelectedCategories { get; set; }
 }
+
