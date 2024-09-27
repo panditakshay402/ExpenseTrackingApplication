29d715edca99c816bea8ccdde1c269a512569bba
JaroslawX <ad.jaro@wp.pl>
2024-09-27 14:31:46 +0200

    refactor

--- ExpenseTrackingApplication/Interfaces/IBudgetCategoryRepository.cs
+++ ExpenseTrackingApplication/Interfaces/IBudgetCategoryRepository.cs
@@ -7,8 +7,6 @@ public interface IBudgetCategoryRepository
     Task<IEnumerable<BudgetCategory?>> GetAllAsync();
     Task<BudgetCategory?> GetByIdAsync(int id);
     Task<IEnumerable<BudgetCategory>> GetByBudgetIdAsync(int budgetId);
-    // Task<bool> UpdateCurrentAmountAsync(int budgetId);
-    // Task<bool> CheckExpensesExceedingLimitAsync(int budgetId);
     Task<bool> AddAsync(BudgetCategory budgetCategory);
     Task<bool> DeleteAsync(BudgetCategory budgetCategory);
     Task<bool> UpdateAsync(BudgetCategory budgetCategory);
