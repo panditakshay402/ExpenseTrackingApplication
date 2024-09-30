3379096ac5c6fef15d8dc41e7051dd7a2fe26aa0
JaroslawX <ad.jaro@wp.pl>
2024-09-30 22:12:26 +0200

    Update report vm

--- ExpenseTrackingApplication/ViewModels/ReportViewModel.cs
+++ ExpenseTrackingApplication/ViewModels/ReportViewModel.cs
@@ -5,7 +5,8 @@ namespace ExpenseTrackingApplication.ViewModels;
 public class ReportViewModel
 {
     public int ReportId { get; set; }
-    public string ReportName { get; set; }
+    public string? ReportName { get; set; }
     public DateTime CreatedAt { get; set; }
     public string ReportType { get; set; }
+    public string Data { get; set; }
 }
