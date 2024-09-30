6ca50d5825f531ad16b2bb6a1fd3fc579a635887
JaroslawX <ad.jaro@wp.pl>
2024-09-30 12:53:37 +0200

    remove bc categorytype enum

--- ExpenseTrackingApplication/Data/Enum/BudgetCategoryType.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ExpenseTrackingApplication.Data.Enum;
-
-public enum BudgetCategoryType
-{
-    Expense,
-    Income,
-    Saving,
-    // TODO: jak będzie potrzeba rozszerzamy, np. Investment, Other etc.
-}
