a4dd27ba32a6e40f61e7789b04483dbbcc32caa6
JaroslawX <ad.jaro@wp.pl>
2024-10-11 11:51:50 +0200

    Support sites

---
+++ ExpenseTrackingApplication/Controllers/SupportController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ExpenseTrackingApplication.Controllers;
+
+public class SupportController : Controller
+{
+    // GET: Support/Index
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    // GET: Support/Contact
+    public IActionResult Contact()
+    {
+        return View();
+    }
+
+    // GET: Support/FAQ
+    public IActionResult FAQ()
+    {
+        return View();
+    }
+}
